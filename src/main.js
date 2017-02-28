9bc38998b421d13f5f2be436cf889286f9766dd3
Felipe <felipequiros@Felipes-MacBook-Pro.local>
2017-02-27 21:29:16 -0500

    Felipe - added config files to build es6 with babel-gulp preset 2015

---
+++ src/main.js
@@ -0,0 +1,20 @@
+/* Go and write your es6 code 
+* i'll keep this file just with commented blocks
+* just uncomment whatever you want to try 
+* or feel free to write your own 
+* Happy hack */
+
+
+
+ /*
+// just a new let
+let test = 'Hello world . . . ';
+*/
+
+/*
+// just a simple class with es6
+class User{
+    register(){
+        console.log('user registered...');
+    }
+}*/
